3c7a33914e46b51dfe065825514e9263bee1d008
David Whiting <david.whiting@h2o.ai>
2019-09-10 18:30:38 -0500

    revert to chapter 1

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,5 +3,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'David'}
-    return render_template('index.html', title='Home', user=user)
+    return "Hello, World!"
