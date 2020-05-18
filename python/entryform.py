fddbd3a97ce8f053042e99117c736d2b2a982752
David Whiting <david.whiting@h2o.ai>
2020-05-18 10:03:43 -0500

    updates

---
+++ python/entryform.py
@@ -0,0 +1,76 @@
+#import PySimpleGUIWeb as sg
+#import PySimpleGUIWx as sg
+
+import PySimpleGUI as sg
+import sqlalchemy as db
+import pandas as pd
+
+## Display a 
+
+engine = db.create_engine('mysql://root:root@127.0.0.1:8306/certification')
+conn = engine.connect()
+metadata = db.MetaData()
+metadata.reflect(bind=engine)
+
+query = """
+        SELECT id, question, answer, correct, randomize, type, aota, nota, enabled 
+        FROM question_templates
+        WHERE enabled=0
+        """
+result = conn.execute(query).fetchall()
+df = pd.DataFrame(result)
+df.columns = result[0].keys()
+
+header_list = df.columns
+data = df.values.tolist()
+
+sg.set_options(auto_size_buttons=True)
+
+layout = [
+    [sg.Table(values=data,
+              headings=df.columns,
+              display_row_numbers=False,
+              auto_size_columns=True,
+              num_rows=min(250, len(data)))]
+    ]
+
+window = sg.Window('Table', layout, font='Default 14', grab_anywhere=False)
+event, values = window.read()
+window.close()
+
+#
+#class Usarios:
+#    def __init__(self, isusario, nome, telefone, email, usario, senha):
+#        pass
+#
+#    def insertUser(self):
+#        return 'User inserted'
+#
+#layout = [  [sg.Text('Question:', size=(25,1), justification='center', font=("Verdana", "12", "bold"))],
+#            [sg.Text('ID:', size=(10,1), justification='right'), sg.Input(size=(14,1), key='-ID-', do_not_clear=False), sg.Button('Search')],
+#            [sg.T('Name:', size=(10,1), justification='right'), sg.I(key='-NAME-', do_not_clear=False)],
+#            [sg.T('Cell:', size=(10,1), justification='right'), sg.I(key='-CELL-', do_not_clear=False)],
+#            [sg.T('E-mail:', size=(10,1), justification='right'), sg.I(key='-EMAIL-', do_not_clear=False)],
+#            [sg.T('User:', size=(10,1), justification='right'), sg.I(key='-USER-', do_not_clear=False)],
+#            [sg.T('Senha:', size=(10,1), justification='right'), sg.I(key='-SENHA-', do_not_clear=False)],
+#            [sg.T(' '*8), sg.Button('Inserir'), sg.Button('Alterar'), sg.Button('Excluir')],
+#            [sg.T(key='-MESSAGE-', size=(30,1), font=("Verdana", "9", "italic"))],
+#            [sg.Text('Busca realizda com sucesso!',size=(30,1), justification='center', font=("Verdana", "10", "italic"))]]
+#
+#window = sg.Window('Question Template Edit', layout, font='Calibri 12', default_element_size=(25,1))
+#
+#while True:             # Event Loop
+#    event, values = window.read()
+#    print(event, values)
+#    if event is None:
+#        break
+#    if event == 'Inserir':
+#        user = Usarios(isusario=values['-USARIO-ID-'],
+#                       nome=values['-NAME-'],
+#                       telefone=values['-TELE-'],
+#                       email=values['-EMAIL-'],
+#                       usario=values['-USARIO-'],
+#                       senha=values['-SENHA-'])
+#        window['-MESSAGE-'].Update(user.insertUser())
+#    else:
+#        window['-MESSAGE-'].Update('Not yet implemlented')#
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,12 +3,39 @@
 ## difficulty might be different for different certification tests
 ## create a different difficulty table
 
-// Creating tables
 
+DROP TABLE IF EXISTS `test_definitions`;
+CREATE TABLE `test_definitions`(
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `description` TEXT, 
+    `dai_version` VARCHAR(15) DEFAULT '1.8.5.1',
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
+    `last_modified` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP    
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `test_subqueries`;
+CREATE TABLE `test_subqueries`(
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `definition_id` INT,
+    `section` INT DEFAULT 1,
+    `prefix` VARCHAR(50) DEFAULT 'SELECT id FROM question_templates ',
+    `definition` VARCHAR(255) DEFAULT '',
+    `randomize` BOOLEAN DEFAULT TRUE,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
+    `last_modified` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX `definition_id` (`definition_id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `test_subqueries` 
+    ADD FOREIGN KEY (`definition_id`) 
+        REFERENCES `test_definitions`(`id`) 
+        ON DELETE RESTRICT 
+        ON UPDATE RESTRICT;
 
 DROP TABLE IF EXISTS `question_templates`;
 CREATE TABLE `question_templates`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `question` TEXT NOT NULL,
     `answer` TEXT,
     `correct` INT DEFAULT 1,
@@ -19,60 +46,43 @@ CREATE TABLE `question_templates`(
         'Numeric',
         'String'
     ) NULL DEFAULT NULL,
-    `tags` SET (
-        'Easy',
-        'Medium',
-        'Hard',
-        'AutoViz',
-        'Datasets',
-        'Experiments',
-        'Expert Settings',
-        'MLI',
-        'Models',
-        'Recipes',
-        'Scorers',
-        'Scoring Pipeline',
-        'Shapley',
-        'Time Series'
-    ) NULL DEFAULT NULL,
     `aota` BOOLEAN DEFAULT FALSE COMMENT '\"All of the above\"' ,
     `nota` BOOLEAN DEFAULT FALSE COMMENT '\"None of the above\"' ,
     `epsilon` REAL DEFAULT 0.0001,
     `enabled` BOOLEAN NOT NULL DEFAULT FALSE,
+    `comments` TEXT,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `last_modified` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(`id`),
     INDEX `enabled`(`enabled`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `question` TEXT NOT NULL,
     `answer` TEXT,
     `item` INT UNSIGNED NOT NULL,
     `test_id` INT UNSIGNED NOT NULL,
     `template_id` INT UNSIGNED NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,    
-    PRIMARY KEY(`id`),
     INDEX `test_id`(`test_id`),
     INDEX `template_id` (`template_id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `datasets`;
 CREATE TABLE `datasets`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50),
     `path` TEXT,
     `train_fraction` DECIMAL,
     `seed` INT,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `student_answers`;
 CREATE TABLE `student_answers`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `student_id` INT,
     `questions_id` INT,
     `score` REAL,
@@ -80,23 +90,15 @@ CREATE TABLE `student_answers`(
     PRIMARY KEY(`id`),
     INDEX(`questions_id`),
     INDEX(`student_id`)
-);
+) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `test_definitions`;
-CREATE TABLE `test_definitions`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(50),
-    `definition` TEXT,
-    `description` TEXT, 
-    `dai_version` VARCHAR(15),
-    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
-    `last_modified` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,    
-    PRIMARY KEY(`id`)
-);
+
+
+
 
 DROP TABLE IF EXISTS `tests`;
 CREATE TABLE `tests`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `definition_id` INT,
     `seed` BIGINT,
     `question_list` TEXT,
@@ -104,29 +106,29 @@ CREATE TABLE `tests`(
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`),
     INDEX(`definition_id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `tag_list`;
 CREATE TABLE `tag_list`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50),
     PRIMARY KEY(`id`),
     UNIQUE(`name`)    
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `templates_id` INT,
     `taglist_id` INT,
     PRIMARY KEY(`id`),
     INDEX(`templates_id`),
     INDEX(`taglist_id`)    
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `exp_definitions`;
 CREATE TABLE `exp_definitions`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50),
     `dataset_id` INT,
     `accuracy` INT,
@@ -137,11 +139,11 @@ CREATE TABLE `exp_definitions`(
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`),
     INDEX(`dataset_id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `experiments`;
 CREATE TABLE `experiments`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50),
     `seed` BIGINT,
     `dai_version` VARCHAR(15),
@@ -149,22 +151,22 @@ CREATE TABLE `experiments`(
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`),
     INDEX(`definition_id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `results`;
 CREATE TABLE `results`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `experiments_id` INT,
     `field` VARCHAR(50),
     `value` TEXT,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`),
     INDEX(`experiments_id`)
-);
+) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email` TEXT,
     `login` VARCHAR(50),
     `password` VARCHAR(50),
@@ -173,5 +175,5 @@ CREATE TABLE `students`(
     `company` VARCHAR(50),
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,   
     PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB;
 
